04453d15d847db9bcdf88827a3208dc118a79104
Mike Solomon <solo4857@vandals.uidaho.edu>
2011-01-23 20:29:13 -0800

    Cleaned up some comments

--- helper.cpp
+++ helper.cpp
@@ -4,47 +4,18 @@
 #include <QPoint>
 #include <assert.h>
 Helper::Helper() {
-	//QLinearGradient gradient(QPointF(50, -20), QPointF(80, 20));
-	//gradient.setColorAt(-1.0, QColor(200, 255, 200)); //Qt::white);
-	//gradient.setColorAt(1.0, QColor(0xa6, 0xce, 0x39));
-
-
 	background = QBrush(QColor(20, 155, 20));
-	//circleBrush = QBrush(gradient);
 	hyenaColor = QBrush(QColor(139, 69, 19));
 	lionColor = QBrush(QColor(220, 220, 20));
 	zebraColor = QBrush(QColor(220, 20, 20));
-	//circlePen = QPen(Qt::black);
-	//circlePen.setWidth(1);
+
 	textPen = QPen(Qt::white);
 	textFont.setPixelSize(50);
-	/*
-	 hyenas.append(QPoint(20,20));
-	 hyenas.append(QPoint(80,80));
-	 lions.append(QPoint(100,30));
-	 zebras.append(QPoint(80,100));
-	 */
 }
 
 void Helper::paint(QPainter *painter, QPaintEvent *event, int elapsed) {
 	painter->fillRect(event->rect(), background);
-	//painter->save();
-	//painter->setPen(circlePen);
-	/*     painter->translate(100, 100);
 
-	 painter->save();
-	 painter->rotate(elapsed * 0.030);
-
-	 qreal r = elapsed/1000.0;
-	 int n = 30;
-	 for (int i = 0; i < n; ++i) {
-	 painter->rotate(30);
-	 qreal radius = 0 + 120.0*((i+r)/n);
-	 qreal circleRadius = 1 + ((i+r)/n)*20;
-	 painter->drawEllipse(QRectF(radius, -circleRadius,
-	 circleRadius*2, circleRadius*2));
-	 }
-	 */
 	painter->setBrush(zebraColor);
 	for (int i = 0; i < 1; i++) {
 		if( !zebras[i].isEmpty()){
@@ -66,11 +37,4 @@ void Helper::paint(QPainter *painter, QPaintEvent *event, int elapsed) {
 			painter->drawEllipse(hyenas[i].dequeue(), 8, 8);
 		}
 	}
-
-	//painter->restore();
-	/*
-	 painter->setPen(textPen);
-	 painter->setFont(textFont);
-	 painter->drawText(QRect(-50, -50, 100, 100), Qt::AlignCenter, "Qt");
-	 */
 }
