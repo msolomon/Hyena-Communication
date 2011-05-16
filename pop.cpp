5de8625b554befdab3f2bbd5be6d9b0f3973946f
Mike Solomon <solo4857@vandals.uidaho.edu>
2011-05-16 11:55:32 -0700

    Fixed integer overflow

--- pop.cpp
+++ pop.cpp
@@ -189,27 +189,7 @@ void pop::island_reproduce(){
         the_pop[rs1]->mutate(i,hyena);
         evaluate_team(rs1,0);
     }
-//    //select investigators
-//    for(int i=0; i < NUM_LIONS; i++){
-//        pi1 = member_select(1,i,investigator);
-//        do{
-//            ri1 = member_select(-1,i,investigator);
-//        }while(ri1 == pi1);
-//        do{
-//            pi2 = member_select(1,i,investigator);
-//        }while(pi2 == pi1 || pi2 == ri1);
-//        do{
-//            ri2 = member_select(-1,i,investigator);
-//        }while(ri2 == ri1 || ri2 == pi1 || ri2 == pi2);
 
-//        the_pop[ri1]->copy(the_pop[pi1],i,investigator);  // investigator
-//        the_pop[ri2]->copy(the_pop[pi2],i,investigator);  // investigator
-//        the_pop[ri1]->calc_size();
-//        the_pop[ri2]->calc_size();
-//        the_pop[ri1]->xOver(the_pop[ri2],i,investigator);
-//        the_pop[ri1]->mutate(i,investigator);
-//        evaluate_team(ri1,0);
-//    }
 }
 
 /*
