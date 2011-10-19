9bb0469a0147398c338d8719e85d283b3f96051a
Mike Solomon <solo4857@vandals.uidaho.edu>
2011-10-19 10:54:51 -0700

    Fixed column output names

--- pop.cpp
+++ pop.cpp
@@ -79,6 +79,11 @@ void pop::write_data(int trial){
 		 "last_pen named landmark "
 		 // nonterminals
 		 "sum subtract compare invert iflteMAG iflteCLOCKWISE ifVectorZero ";
+	// hyena inputs (also terminals)
+	for(int i = 1; i < NUM_HYENA_INPUTS; i++){
+		f << "h_" << i << " ";
+	}
+	// individual hyena fitnesses (not an input at all)
 	for(int i = 1; i < NUM_HYENAS; i++){
 		f << "h" << i << " ";
 	}
