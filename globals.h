a4df374822495f3ab6b05f6ac959705ee3498bb5
Mike Solomon <solo4857@vandals.uidaho.edu>
2012-01-09 14:44:44 -0800

    Restructured global settings file

--- globals.h
+++ globals.h
@@ -12,70 +12,47 @@ using RandomLib::Random;
 
 #include "types.h"
 
-//// Common settings
+//// Settings
 const int GENERATIONS = 1500;
 const int POP_SIZE = 100;
-const int NUM_HYENAS = 14;
-const int NUM_HYENA_INPUTS = 0; // 0 to disable, otherwise first N
-const bool VEC_CALLING_ONLY = false; // vectors to all calling, or all period?
 const int NUM_LIONS = 2;
+const int NUM_HYENAS = 14;
 const int TIME_STEPS = 100;
 const int NUM_TESTS = 5; // times to repeat tests to prevent luck
-const bool FINAL_TEST_MEAN = false; // force mean fitness to pick final best team
+const int NUM_HYENA_INPUTS = 0; // 0 to disable, otherwise first N
+const bool VEC_CALLING_ONLY = false; // vectors to all calling, or all period?
 const int FINAL_TESTS = 2000; // number of times to run best hyena of last trial
-const int KNOCKOUT_TESTS = 2000; // as above, but with a given inputs disabled
+const int KNOCKOUT_TESTS = 2000; // as above, but with given inputs disabled
 const bool LIONS_RETURN = false; // lions return to kill if close and few hyenas
 const double PARSIMONY_COEFF = 0.001;
-const bool USE_90_10_XOVER = true;
-const bool ALLOW_INTERNAL_MUTATION = true;
 const ops DISABLED_OPS[] = {};
 const ops KNOCKOUT_OPS[] = {};
-const selection_method SELECTION_METHOD = mean;
-const char* const RETEST_GIVEN = NULL; // NULL to disable, else filename
+const selection_method SELECTION_METHOD = mean; // mean, median, minimum, maximum
+const bool FINAL_TEST_MEAN = false; // force mean fitness to pick final best team
 // 0 to disable, else start hyenas within X units: exactly 1 non-named inside
 // the calling radius
 const double RADIUS_START = 16;
-// Only pick one method below
-const bool OET_GENERATIONAL = false; // 50% team gen., 50% island gen.
-const bool TEAM_GENERATIONAL = false; // 100% team generational
-const bool INDIV_GENERATIONAL = true; // 100% island generational
-const bool ISLAND_STEADY = false;
-// GUI/Drawing settings
-const bool GUI = false;
-const bool DRAW = true; // if false, videos are unavailable
-const int DRAW_EVERY = 10; // draw every X generations
-const bool ANTIALIAS = true; // antialias GUI drawing
-const bool HYENA_MARKERS = false; // draw short line to ID different hyenas
-const bool SMEAR_DRAW = true; // don't erase background between timesteps
-const int SMEAR_LAST = 10; // erase timesteps older than this; 0 for never
-const bool FADE_DRAW = true; // hyenas less opaque toward end (more overlap)
-const bool FADE_OUT = false; // older positions fade with time
-const double ANIMAL_MAX_OPACITY = 0.6;
-const double ANIMAL_MIN_OPACITY = 0.1;
 
-// Use %1 for trial number
-const char* const VIDEO_TEMPLATE = "video_%1.txt";
-const char* const DATA_TEMPLATE = "data_%1.txt";
-const char* const GRAPHVIZ_TEMPLATE = "hyena_%1.gv";
-const char* const FINAL_TEMPLATE = "final_%1.txt";
-const char* const FINAL_VIDEO_TEMPLATE = "finalvideo_%1.txt";
-const char* const KNOCKOUT_TEMPLATE = "knockout_%1.txt";
-const char* const KNOCKOUT_VIDEO_TEMPLATE = "knockoutvideo_%1.txt";
-const char* const BEST_TEAM_TEMPLATE = "bestteam_%1.txt";
-
-//// Other settings
-// Selection and reproduction
+// Selection, crossover, mutation, initial trees
 const int TOURNAMENT_SIZE = 3;
-const double MUTATION_CHANCE = 1.0; // % chance of mutation
+const double MUTATION_CHANCE = 1.0; // flat % chance of mutation
    // NUM_OVER... overrides above if nonzero. gives X/size chance mut. per node
 const double NUM_OVER_SIZE_MUTATION = 1.0;
 const bool SPECIALIZED_CONST_MUT = true;
 const double CONST_MUT_MAG_SIGMA = 1.0;
 const double CONST_MUT_DIR_SIGMA = 1.0;
+const bool USE_90_10_XOVER = true;
+const bool ALLOW_INTERNAL_MUTATION = true;
 const bool FULL = true; // false = GROW (for initial trees)
 const int GROW_DEPTH = 4;
 const int TRIALS = 1; // leaks memory; do via script
-const int TREE_MAX_SIZE = INT_MAX; // size, not depth
+const int TREE_MAX_SIZE = INT_MAX; // size, not depth: INT_MAX for unlimited
+// Only pick one method below
+const bool OET_GENERATIONAL = false; // 50% team gen., 50% island gen.
+const bool TEAM_GENERATIONAL = false; // 100% team generational
+const bool INDIV_GENERATIONAL = true; // 100% island generational
+const bool ISLAND_STEADY = false;
+
 // Model
 const int X = 32;
 const int Y = X;
@@ -85,7 +62,7 @@ const bool CALLING_ENABLED = true;
 const double CALLING_RANGE = 8; // hyenas call when close to zebra
 const bool START_ONE_INSIDE = true; // at least one hyena inside calling radius
 const bool START_OUTSIDE_ZEBRA = false; // doesn't apply to radius placement
-const double BASE_IMPORTANCE = 5000.0;
+const double BASE_IMPORTANCE = 5000.0; // scales measure of ops usefulness
 const double LION_ATTACK_RADIUS = 3;
 const double LION_ATTACK_PENALTY = 3;
 const double LION_LION_RADIUS = 5;
@@ -104,7 +81,36 @@ const double LION_HYENA_RADIUS_SQ = LION_HYENA_RADIUS * LION_HYENA_RADIUS;
 const double HYENA_HYENA_RADIUS_SQ = HYENA_HYENA_RADIUS * HYENA_HYENA_RADIUS;
 const double CALLING_RANGE_SQ = CALLING_RANGE * CALLING_RANGE;
 // General constants
-const double PI = 3.141592654;
+const double PI = 3.1415926535897932384626433832795028842;
+
+//// GUI/Drawing settings
+// Set GUI to true to enable visualizer; set to false for running simulations
+const bool GUI = false;
+const bool DRAW = true; // if false, videos are unavailable
+const int DRAW_EVERY = 10; // draw every X generations
+const bool ANTIALIAS = true; // antialias GUI drawing
+const bool HYENA_MARKERS = false; // draw short line to ID different hyenas
+const bool SMEAR_DRAW = true; // don't erase background between timesteps
+const int SMEAR_LAST = 10; // erase timesteps older than this; 0 for never
+const bool FADE_DRAW = true; // hyenas less opaque toward end (more overlap)
+const bool FADE_OUT = false; // older positions fade with time
+const double ANIMAL_MAX_OPACITY = 0.6;
+const double ANIMAL_MIN_OPACITY = 0.1;
+
+//// Output file paths
+// Use %1 for trial number
+const char* const VIDEO_TEMPLATE = "video_%1.txt"; // video output
+const char* const DATA_TEMPLATE = "data_%1.txt"; // data output
+const char* const GRAPHVIZ_TEMPLATE = "hyena_%1.gv"; // graphviz output
+const char* const FINAL_TEMPLATE = "final_%1.txt"; // last gen. data output
+const char* const FINAL_VIDEO_TEMPLATE = "finalvideo_%1.txt"; // l.g. video out
+const char* const KNOCKOUT_TEMPLATE = "knockout_%1.txt"; // knockout data out
+const char* const KNOCKOUT_VIDEO_TEMPLATE = "knockoutvideo_%1.txt"; // k.o. video out
+const char* const BEST_TEAM_TEMPLATE = "bestteam_%1.txt"; // best team output
+// set RETEST_GIVEN path for input team (dumped by BEST_TEAM_TEMPLATE)
+const char* const RETEST_GIVEN = NULL; // NULL to disable, else filename
+// seed output is "seed_%s.txt" where %s is argv[1]
+
 
 //// Global functions
 inline double dist(double x, double y){  // 'distance' is a built-in function
